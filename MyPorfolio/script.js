fde50e0b6c52b647e161eb18236a05beae64f259
TheGlitch26 <162802564+TheGlitch26@users.noreply.github.com>
2025-12-11 18:46:46 +0400

    Update script.js. Fixed sticky disapperaing menu links

--- MyPorfolio/script.js
+++ MyPorfolio/script.js
@@ -1,5 +1,6 @@
+let hamburgerDiv = document.querySelector('.js-hamburger-box');
+
 function toggleNav(){
-  let hamburgerDiv = document.querySelector('.js-hamburger-box');
   if(hamburgerDiv.classList.contains('js-hamburger-box-active')){
     hamburgerDiv.classList.remove('js-hamburger-box-active');
   }
@@ -7,3 +8,7 @@ function toggleNav(){
     hamburgerDiv.classList.add('js-hamburger-box-active');
   }
 }
+
+window.addEventListener("resize", () => {
+  hamburgerDiv.classList.remove("js-hamburger-box-active");
+});
